39b09ef4d8b5573c91aebbdc9c96c505d35ed2c6
chengxugang <xyz0z0.xyz@gmail.com>
2024-03-29 15:35:41 +0800

    add library

--- app/src/main/java/xyz/xyz0x0/rxjavasample/MainActivity.java
+++ app/src/main/java/xyz/xyz0x0/rxjavasample/MainActivity.java
@@ -2,6 +2,11 @@ package xyz.xyz0x0.rxjavasample;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    test1();
+  }
+
+
+  private void test1() {
+    Observable.just(1)
+        .observeOn(Schedulers.io())
+        .subscribe(new Consumer<Integer>() {
+          @Override public void accept(Integer integer) throws Throwable {
+
+          }
+        });
   }
 }
